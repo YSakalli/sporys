680b6585dfd361603de81ca99232b5dc10eb4ea2
YSakalli <ysakallidev@gmail.com>
2023-11-23 12:05:57 +0300

    Profile

--- profileuser.php
+++ profileuser.php
@@ -21,6 +21,8 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
         $sql = "UPDATE users SET username= '$usernamenew' WHERE id=$userID";
 
         $query = mysqli_query($conn, $sql);
+        header("Location:exit.php")
+        exit();
     }
     
 
