ff4d0f49601269d1e6df552f92e431289e4725f2
YSakalli <ysakallidev@gmail.com>
2023-11-25 13:42:24 +0300

    response 1.0

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header")
     header.classList.toggle("active", window.scrollY > 0);
 })
+
+let cards = document.querySelectorAll('.card');
+
+cards.forEach(function (card) {
+    card.addEventListener('click', function () {
+
+        cards.forEach(function (c) {
+            c.classList.remove('active');
+        });
+
+        card.classList.add('active');
+    });
+});
