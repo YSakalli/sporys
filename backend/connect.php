d7b8a5fae86628fc9baa22926ff4a7a338362e0e
YSakalli <ysakallidev@gmail.com>
2023-12-08 20:19:00 +0300

    fa

--- backend/connect.php
+++ backend/connect.php
@@ -1,7 +1,10 @@
 <?php
 $servername = "db";
+
 $username = "dbtecnoc_yusuf";
+
 $password = "6NY?]He4V#K3";
+
 $dbname = "dbtecnoc_sporys";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
